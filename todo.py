e0ff87087f660d51eb5a40c0e59ce5616bb8dbe2
Vishnu4712 <vishnupvt51@gmail.com>
2025-03-23 16:42:03 +0530

    Fixed Bug in the code

--- todo.py
+++ todo.py
@@ -10,7 +10,26 @@ def list_tasks():
     else:
         for idx, task in enumerate(tasks, start=1):
             print(f'{idx}. {task}')
-            
+
+def remove_task(task_number):
+    if not tasks:
+        print("No tasks to remove.")
+        return
+
+    if 1 <= task_number <= len(tasks):
+        removed_task = tasks.pop(task_number - 1)
+        print(f'Task \"{removed_task}\" removed.')
+    else:
+        print("Invalid task number. Please enter a valid number.")
+
 if __name__ == "__main__":
     add_task("Finish Assignment")
+    add_task("Submit Report")
     list_tasks()
+
+    print("\nTrying to remove Task 1...")
+    remove_task(1)
+    list_tasks()
+
+    print("\nTrying to remove an invalid task...")
+    remove_task(5)  # This will now show a proper error message.
