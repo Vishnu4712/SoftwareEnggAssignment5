6dd1c629be4e029171db1cd3349769d7b91507d5
Vishnu4712 <vishnupvt51@gmail.com>
2025-03-23 16:28:46 +0530

    Added Remove Task Feature

--- todo.py
+++ todo.py
@@ -10,7 +10,19 @@ def list_tasks():
     else:
         for idx, task in enumerate(tasks, start=1):
             print(f'{idx}. {task}')
-            
+
+def remove_task(task_number):
+    if 1 <= task_number <= len(tasks):
+        removed_task = tasks.pop(task_number - 1)
+        print(f'Task "{removed_task}" removed.')
+    else:
+        print("Invalid task number.")
+
 if __name__ == "__main__":
     add_task("Finish Assignment")
+    add_task("Submit Report")
+    list_tasks()
+    
+    print("\nRemoving Task 1...")
+    remove_task(1)
     list_tasks()
